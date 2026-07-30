abfac6ac563809b5e49373848c876f21400a7439
Muhammad Ahsan <ahsanexpert16@gmail.com>
2026-07-30 15:51:30 +0500

    adding google ad sense meta tag for verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-2722637490463810"></meta>
         <link rel="preconnect" href="https://api.fontshare.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://api.fontshare.com" />
         <link
